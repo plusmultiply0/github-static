0730c8db76ee7a79e84d4359fe4f0c4e73382738
jczhou <1051160112@qq.com>
2020-03-28 14:35:35 +0800

    finish a search menu

--- scripts/one.js
+++ scripts/one.js
@@ -1,13 +1,23 @@
 /*顶部搜索框点击*/
+/*显示下拉搜索框 */
 var topSearchClick = document.getElementsByClassName('topsearch')[0];
 var topSearchImg = document.getElementsByClassName('searchImg')[0];
 var topSearchInput = document.getElementsByClassName('searchin')[0];
 
+var btsearchinput = document.getElementsByClassName('btsearch')[0];
+var getSearchValue = document.getElementsByClassName('searchin')[0];
+var showValuein = document.getElementsByClassName('forvalue')[0];
+var getFormerValue = document.getElementsByClassName('forvalue');
+
+
+
 var turnWhite = function(){
     topSearchImg.style.display="none";
-    topSearchClick.style.width = "400px";
+    topSearchClick.style.width = "460px";
     topSearchClick.style.background = "white";
     topSearchInput.style.width="390px";
+
+    btsearchinput.style.display="flex";
 }
 
 topSearchClick.addEventListener("click",turnWhite);
@@ -18,8 +28,48 @@ var cancelTopClick = function(){
     topSearchClick.style.width = "300px";
     topSearchClick.style.background = "hsla(0,0%,100%,.125)";
     topSearchInput.style.width="288px";
+
+    btsearchinput.style.display="none"
 }
 topSearchClick.addEventListener('focusout',cancelTopClick);
+
+/*在搜索框下方实时显示输入结果 */
+var showSearchResult = function(event){
+    var putValue = document.getElementsByClassName('searchvalue')[0];
+    console.log(putValue.value);
+    var showValue = document.getElementsByClassName('searchin')[0];
+    console.log(showValue.value);
+    if(!showValue.value){
+        putValue.value="";
+    }
+    else{
+        putValue.value = showValue.value;
+    }
+    
+}
+/*
+var showSearchResult = function(event){
+    if( btsearchinput.style.display == "flex"){
+        var getValue = document.getElementsByClassName('searchin')[0];
+        var getformerValue = document.getElementsByClassName('forvalue');
+
+        var value = getValue.value;
+        console.log(value);
+        //showValuein.removeChild(nowValue);
+        var nowValue = document.createTextNode(value);
+
+        if(!getformerValue[0].firstChild.wholeText){
+            showValuein.appendChild(nowValue);
+        }
+
+        var formervalue = getFormerValue[0].firstChild;
+        console.log('yes');
+
+        showValuein.replaceChild(nowValue,formervalue);
+    }
+}
+*/
+topSearchClick.addEventListener('keyup',showSearchResult);
 
 /*顶部+号点击*/
 var topRightadd = document.getElementById('addicon');
